0d50d2bcb2b5b4017c741d20710b96b38ff2bed0
anhuarMI <122800609+anhuar130204@users.noreply.github.com>
2023-11-13 19:16:36 -0600

    Update insertar.js

--- static/js/insertar.js
+++ static/js/insertar.js
@@ -1,6 +1,6 @@
 function insertar(email, nombre, telefono) {
     var request = new XMLHttpRequest();
-    request.open('POST', "http://127.0.0.1:8000/contactos");
+    request.open('POST', "https://contactos-back-3955e37c5233.herokuapp.com/contactos");
     request.setRequestHeader("Content-Type", "application/json");
 
     // Crear un objeto con los parámetros a enviar
