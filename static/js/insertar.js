56bd111d1241d227b65cd5864a845cca9ce92cda
anhuarMI <122800609+anhuar130204@users.noreply.github.com>
2023-11-30 12:32:51 -0600

    Update insertar.js

--- static/js/insertar.js
+++ static/js/insertar.js
@@ -1,9 +1,12 @@
-function insertar(email, nombre, telefono) {
+function insertar() {
+    var email = document.getElementById('email').value;
+    var nombre = document.getElementById('nombre').value;
+    var telefono = document.getElementById('telefono').value;
+
     var request = new XMLHttpRequest();
     request.open('POST', "https://contactos-back-3955e37c5233.herokuapp.com/contactos");
     request.setRequestHeader("Content-Type", "application/json");
 
-    // Crear un objeto con los parámetros a enviar
     var data = {
         email: email,
         nombre: nombre,
@@ -12,23 +15,19 @@ function insertar(email, nombre, telefono) {
 
     request.send(JSON.stringify(data));
 
-    request.onload = (e) => {
+    request.onload = function () {
         if (request.status === 200) {
-            // La solicitud se realizó con éxito
             const response = JSON.parse(request.responseText);
-            console.log("Response:", response);
-            // Muestra una alerta para indicar que la inserción se realizó con éxito
+            console.log("Respuesta:", response);
             alert("Contacto insertado correctamente");
         } else {
             console.error("Error al hacer la solicitud:", request.status, request.statusText);
-            // Muestra una alerta para indicar que ocurrió un error
             alert("Error al insertar el contacto. Por favor, inténtalo de nuevo.");
         }
     };
 }
+
 function cancelar() {
-            
     window.location.href = "/";
-    
     alert("Acción cancelada");
 }
